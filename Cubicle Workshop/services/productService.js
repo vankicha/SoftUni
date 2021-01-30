83a12f37eaf28e29618431e22790835a7b2fd097
vankicha <ivanip.burgas@gmail.com>
2021-01-30 19:20:00 +0200

    Add filtering

--- Cubicle Workshop/services/productService.js
+++ Cubicle Workshop/services/productService.js
@@ -2,7 +2,25 @@ const Cube = require('../models/Cube');
 const Accessory = require('../models/Accessory');
 
 async function getAll({ search, from, to }) {
-    let result = await Cube.find().lean();
+    
+    let result;
+    
+    if (search || from || to) {
+        search = search.length > 0 ? search : '';
+        from = from ? +from : 1;
+        to = to ? +to : 6;
+        
+        let regexSearch = new RegExp(search, 'i');
+        result = await Cube
+            .find({ name: { $regex: regexSearch } })
+            .where('difficultyLevel')
+            .gte(+from)
+            .lte(+to)
+            .lean();
+        return result;
+    }
+
+    result = await Cube.find().lean();
 
     return result;
 }
